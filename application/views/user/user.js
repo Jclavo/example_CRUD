12c83828bd9301ff5654eddef6b8507af538f24d
jclavotafur@gmail.com <andaNADA123G>
2018-12-06 21:06:47 -0500

    user

--- application/views/user/user.js
+++ application/views/user/user.js
@@ -4,12 +4,12 @@ var DELETE 	 = 'DELETE'
 var UPDATE 	 = 'UPDATE'
 var READ 	 = 'READ'
 
-var URL_CREATE 	 = 'http://localhost/example_CRUD/index.php/user/create_user'
-var URL_READ_ALL = 'http://localhost/example_CRUD/index.php/user/get_user'
-var URL_DELETE 	 = 'http://localhost/example_CRUD/index.php/user/delete_user'
-var URL_UPDATE 	 = 'http://localhost/example_CRUD/index.php/user/update_user'
+var URL_CREATE 	 = 'http://localhost/example_CRUD/index.php/student/create_student'
+var URL_READ_ALL = 'http://localhost/example_CRUD/index.php/student/get_student'
+var URL_DELETE 	 = 'http://localhost/example_CRUD/index.php/student/delete_student'
+var URL_UPDATE 	 = 'http://localhost/example_CRUD/index.php/student/update_student'
 		
-var global_user = {
+var global_student = {
 		id : '',
 		name : '',
 		email : ''
@@ -17,48 +17,48 @@ var global_user = {
 
 
 $(document).ready(function() {
-	global_user.clear
-	call_ajax(READ_ALL,global_user);
+	global_student.clear
+	call_ajax(READ_ALL,global_student);
 
 });
 
-function add_user() {
+function add_student() {
 
-	clear_global_user()
-	global_user.name  = document.getElementById("user-name").value
-	global_user.email = document.getElementById("user-email").value
+	clear_global_student()
+	global_student.name  = document.getElementById("student-name").value
+	global_student.email = document.getElementById("student-email").value
 	
-	call_ajax(CREATE,global_user);
+	call_ajax(CREATE,global_student);
 }
 
-//Editing user
-function edit_user(id_user) {
+//Editing student
+function edit_student(id_student) {
 
-	clear_global_user()
-	global_user.id = id_user
-	call_ajax(READ,global_user)
+	clear_global_student()
+	global_student.id = id_student
+	call_ajax(READ,global_student)
 	
 }
 
-//Updating user
-function update_user() {
+//Updating student
+function update_student() {
 	
-	clear_global_user()
-	global_user.id 	  = document.getElementById("user-id").value
-	global_user.name  = document.getElementById("user-name").value
-	global_user.email = document.getElementById("user-email").value
+	clear_global_student()
+	global_student.id 	  = document.getElementById("student-id").value
+	global_student.name  = document.getElementById("student-name").value
+	global_student.email = document.getElementById("student-email").value
 		
-	call_ajax(UPDATE,global_user);
+	call_ajax(UPDATE,global_student);
 	
 }
 
-// deleting user
-function delete_user(id_user) {
+// deleting student
+function delete_student(id_student) {
 
-	clear_global_user()
-	global_user.id = id_user
+	clear_global_student()
+	global_student.id = id_student
 	
-	call_ajax(DELETE,global_user)
+	call_ajax(DELETE,global_student)
 }
 
 function call_ajax(operation, data_input) {
@@ -66,7 +66,7 @@ function call_ajax(operation, data_input) {
 	 * $.ajax({ //data : parametros, url :
 	 * favor..."); }, success : function(response) {
 	 * //$("#resultado").html(response); alert(response);
-	 * //alert(response['user']); //users = response; //alert(users.user); } });
+	 * //alert(response['student']); //students = response; //alert(students.student); } });
 	 */
 	var url_operation
 
@@ -98,28 +98,28 @@ function call_ajax(operation, data_input) {
 	 // data : { name : 'xd', email : 'xd.xmom' }
 	
 	}).done(function(data) {
-		// alert(data.user);
+		// alert(data.student);
 		console.log(data)
 		
-		clear_global_user()
+		clear_global_student()
 		switch (operation) {
 		case CREATE:
 			alert(data.status)
-			call_ajax(READ_ALL,global_user)
+			call_ajax(READ_ALL,global_student)
 			break
 		case READ_ALL:
-			display_users(data.user)
+			display_students(data.student)
 			break
 		case DELETE:
 			alert(data.status)
-			call_ajax(READ_ALL,global_user)
+			call_ajax(READ_ALL,global_student)
 			break;
 		case READ:
-			display_user(data.user)
+			display_student(data.student)
 			break
 		case UPDATE:
 			alert(data.status)
-			call_ajax(READ_ALL,global_user)
+			call_ajax(READ_ALL,global_student)
 			break
 		default:
 			break
@@ -128,27 +128,27 @@ function call_ajax(operation, data_input) {
 
 }
 
-function display_users(user) {
+function display_students(student) {
 	
 	//codigo para el boton cambie de nombre a SAVE
 	var crbtn = document.createElement("button")
 	crbtn.innerHTML = "Save";
-	crbtn.setAttribute("onclick", "add_user()")
+	crbtn.setAttribute("onclick", "add_student()")
 	crbtn.setAttribute("class", "btn btn-sm btn-success")
 	document.getElementById("saveupdate").innerHTML = ""
 	document.getElementById("saveupdate").appendChild(crbtn);
 	
 	// codigo para cargar los valores
-	document.getElementById("form-list-user-body").innerHTML = "";
+	document.getElementById("form-list-student-body").innerHTML = "";
 
-	for (i = 0; i < user.length; i++) {
+	for (i = 0; i < student.length; i++) {
 
 		var myTr = document.createElement("tr")
 
-		for (user_field in user[i]) {
+		for (student_field in student[i]) {
 
 			var mytd = document.createElement("td")
-			mytd.innerHTML = user[i][user_field]
+			mytd.innerHTML = student[i][student_field]
 			myTr.appendChild(mytd)
 
 		}
@@ -157,48 +157,49 @@ function display_users(user) {
 		var editBtn = document.createElement("button")
 		editBtn.innerHTML = "Edit"
 		editBtn.setAttribute("class", "btn btn-sm btn-primary")
-		editBtn.setAttribute("onclick", "edit_user(" + user[i]['id'] + ")")
+		editBtn.setAttribute("onclick", "edit_student(" + student[i]['id'] + ")")
 
 		var deletebtn = document.createElement("button")
 		deletebtn.innerHTML = "Delete"
 		deletebtn.setAttribute("class", "btn btn-sm btn-danger")
-		deletebtn.setAttribute("onclick", "delete_user(" + user[i]['id'] + ")")
+		deletebtn.setAttribute("onclick", "delete_student(" + student[i]['id'] + ")")
 
 		actionTd.appendChild(editBtn)
 		actionTd.appendChild(deletebtn)
 		myTr.appendChild(actionTd)
 		
-		document.getElementById("form-list-user-body").appendChild(myTr)
+		document.getElementById("form-list-student-body").appendChild(myTr)
 	}
 	
-	document.getElementById("user-id").value = "";
-	document.getElementById("user-name").value = "";
-	document.getElementById("user-email").value = ""
+	document.getElementById("student-id").value = "";
+	document.getElementById("student-name").value = "";
+	document.getElementById("student-email").value = ""
 
 }
 
-function display_user(user) {
+function display_student(student) {
 	
 	var updatebtn = document.createElement("button")
 	updatebtn.innerHTML = "Update";
 	updatebtn.setAttribute("class", "btn btn-sm btn-success")
-	updatebtn.setAttribute("onclick", "update_user()")
+	updatebtn.setAttribute("onclick", "update_student()")
 
 	document.getElementById("saveupdate").innerHTML = ""
 	document.getElementById("saveupdate").appendChild(updatebtn)
-	document.getElementById("user-id").value = user[0]['id']
-	document.getElementById("user-name").value = user[0]['name']
-	document.getElementById("user-email").value = user[0]['email']
-				
+	document.getElementById("student-id").value = student[0]['id']
+	document.getElementById("student-name").value = student[0]['name']
+	document.getElementById("student-lastname").value = student[0]['lastname']
+	document.getElementById("student-cellphone").value = student[0]['cellphone']
+
 }
 
 
 
-function clear_global_user() {
+function clear_global_student() {
 	
-	global_user.id	  = ''
-	global_user.name  = ''
-	global_user.email = ''
+	global_student.id	  = ''
+	global_student.name  = ''
+	global_student.email = ''
 }
 
 /*
